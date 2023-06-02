bc6d4261a0a5f5d4b95a1a9bc16dbbf50b33b41c
Gerard Downes <gdownes@gmail.com>
2023-06-02 23:14:57 +0100

    Support formdata

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const port = process.env.API_PORT ?? 5001;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded());
 
 function extractCognitoClaims(req: Request) {
     const includeClaims = process.env.API_INCLUDE_COGNITO_CLAIMS
